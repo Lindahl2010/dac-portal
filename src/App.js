2b1288e3149091d7769048742cc85154e9695cb9
Eli Lindahl <linelij@dunwoody.edu>
2021-04-24 16:18:30 -0500

    Updated css and added more info to pages.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom';
 import Header from './components/header/header.js';
 import Main from './pages/Main/main.js';
 import About from './pages/About/about.js';
+import AD_Request from './pages/AD_Request/ad_request';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
 				<Main/>
 			</Route>
 			<Route exact path='/about' component={About}/>
+			<Route exact path='/ad-request' component={AD_Request}/>
 		</Switch>
     </div>
   );
